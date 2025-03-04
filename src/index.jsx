716ce13ea95d7021f97ca0589bf5b0e727094fe3
thorlio <thorlio3@gmail.com>
2025-03-03 23:42:31 -0800

    updated index.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
+import process from "process";
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 import Container from "react-bootstrap/Container";
-// import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
 const App = () => {
